6576ee73ca6d5f53e68b4a2b201d1dcdc25ddce9
Hari Charan Korrapati <haricharank.official@gmail.com>
2024-05-12 08:19:40 +0530

    Replcae with our VSM model

--- Code/informationRetrieval.py
+++ Code/informationRetrieval.py
@@ -1,8 +1,4 @@
 from util import *
-
-
-# Add your import statements here
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -10,91 +6,77 @@ from itertools import chain
 
 class InformationRetrieval():
 
-	def __init__(self):
-		self.index = None
+    def __init__(self):
+        self.index = None
 
-	def buildIndex(self, docs, docIDs):
-		"""
-		Builds the document index in terms of the document
-		IDs and stores it in the 'index' class variable
-		Parameters
-		----------
-		arg1 : list
-			A list of lists of lists where each sub-list is
-			a document and each sub-sub-list is a sentence of the document
-		arg2 : list
-			A list of integers denoting IDs of the documents
-		Returns
-		-------
-		None
-		"""
-		self.count_vectorizer = CountVectorizer()
-		# flattened_documents = [' '.join(sentence) for document in docs for sentence in document]
+    def buildIndex(self, docs, docIDs):
+        """
+        Builds the document index in terms of the document
+        IDs and stores it in the 'index' class variable
+        Parameters
+        ----------
+        arg1 : list
+            A list of lists of lists where each sub-list is
+            a document and each sub-sub-list is a sentence of the document
+        arg2 : list
+            A list of integers denoting IDs of the documents
+        Returns
+        -------
+        None
+        """
+        self.count_vectorizer = CountVectorizer(ngram_range=(1,1))
+        all_docs_combined = []
+        for document in docs:
+            all_sentences_combined = []
+            for sentence in document:
+                all_sentences_combined.extend(sentence)
+            all_docs_combined.append(all_sentences_combined)
+        all_docs_combined = [' '.join(sentence) for sentence in all_docs_combined]
+        self.term_doc_freq = self.count_vectorizer.fit_transform(all_docs_combined)
+        self.docIDs = docIDs
+        self.index = self.term_doc_freq.T
 
-		# # Generate document-term matrix
-		# dtm = self.count_vectorizer.fit_transform(flattened_documents)
-		# vocabulary = self.count_vectorizer.get_feature_names_out()
+    def rank(self, queries):
+        """
+        Rank the documents according to relevance for each query
+        Parameters
+        ----------
+        arg1 : list
+            A list of lists of lists where each sub-list is a query and
+            each sub-sub-list is a sentence of the query
+        
+        Returns
+        -------
+        list
+            A list of lists of integers where the ith sub-list is a list of IDs
+            of documents in their predicted order of relevance to the ith query
+        """
 
-		# document_index = {}
-		# # Loop through each document
-		# documents = docs
-		# for i, doc_id in enumerate(docIDs):
-		# 	document_sentences = [sentence for sentence in documents[i]]
-		# 	document_vectors = []
-		# 	for sentence in document_sentences:
-		# 		sentence_vector = self.count_vectorizer.transform([' '.join(sentence)]).toarray().flatten()
-		# 		document_vectors.append(sentence_vector)
-		# 	document_vector = sum(document_vectors)
-		# 	for j, term in enumerate(vocabulary):
-		# 		if document_vector[j] > 0:
-		# 			if term not in document_index:
-		# 				document_index[term] = []
-		# 			document_index[term].append(doc_id)
-		
-		flattened_documents = [' '.join(list(chain.from_iterable(x))) for x in docs]
-		self.term_doc_freq = self.count_vectorizer.fit_transform(flattened_documents)
-		self.docIDs = docIDs
-		self.index = self.term_doc_freq.T
+        doc_IDs_ordered = []
 
-
-
-	def rank(self, queries):
-		"""
-		Rank the documents according to relevance for each query
-		Parameters
-		----------
-		arg1 : list
-			A list of lists of lists where each sub-list is a query and
-			each sub-sub-list is a sentence of the query
-		
-		Returns
-		-------
-		list
-			A list of lists of integers where the ith sub-list is a list of IDs
-			of documents in their predicted order of relevance to the ith query
-		"""
-
-		doc_IDs_ordered = []
-
-		#Fill in code here
-		term_doc_freq = self.term_doc_freq
-		self.tfidf_transformer = TfidfTransformer(norm='l2',use_idf=True, smooth_idf=False,sublinear_tf=False)
-		self.tfidf_transformer.fit(term_doc_freq)
-		tfidf = self.tfidf_transformer.transform(term_doc_freq)
-
-
-		flattened_queries = [' '.join(list(chain.from_iterable(x))) for x in queries]
-		querycounts = self.count_vectorizer.transform(flattened_queries)
-		querytfidf = self.tfidf_transformer.transform(querycounts)
-
-		cossimilarity = cosine_similarity(querytfidf,tfidf)
-		docIDS = self.docIDs
-
-		for cos_similarity_vector in cossimilarity:
-			top_doc_indices = cos_similarity_vector.argsort()[::-1]
-			top_docs = [docIDS[docidx] for docidx in top_doc_indices]
-			doc_IDs_ordered.append(top_docs)
-
-	
-		return doc_IDs_ordered
-
+        # Fill in code here
+        term_doc_freq = self.term_doc_freq
+        self.tfidf_transformer = TfidfTransformer(norm='l2', use_idf=True, smooth_idf=False, sublinear_tf=False)
+        self.tfidf_transformer.fit(term_doc_freq)
+        tfidf = self.tfidf_transformer.transform(term_doc_freq)
+        all_queries_combined = []
+        for query in queries:
+            all_sentences_combined = []
+            for sentence in query:
+                all_sentences_combined.extend(sentence)
+            all_queries_combined.append(all_sentences_combined)
+        all_queries_combined = [' '.join(sentence) for sentence in all_queries_combined]
+        query_vectorizer = self.count_vectorizer.transform(all_queries_combined)
+        query_vectors = self.tfidf_transformer.transform(query_vectorizer)
+        similarity_values = cosine_similarity(query_vectors, tfidf)
+        for value in similarity_values:
+            scores = []
+            total_docs = len(self.docIDs)
+            for i in range(total_docs):
+                scores.append((self.docIDs[i], value[i]))
+            sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+            ordered_docs_query = []
+            for i in sorted_scores:
+                ordered_docs_query.append(i[0])
+            doc_IDs_ordered.append(ordered_docs_query)
+        return doc_IDs_ordered
