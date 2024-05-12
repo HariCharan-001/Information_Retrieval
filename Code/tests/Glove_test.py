b371e701f84718e28c4fb10e9585bd183f12bd59
Hari Charan Korrapati <haricharank.official@gmail.com>
2024-05-12 08:16:22 +0530

    Calc avg size of cranfiled doc, to decide upon using FAISS

--- Code/tests/Glove_test.py
+++ Code/tests/Glove_test.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.join(dir_path_local, '..'))
 
 import numpy as np
 from Glove import *
+from sentenceSegmentation import SentenceSegmentation
 
 def test_precomputed_embeddings():
     doc_embeddings = np.load(os.path.join(dir_path_local, '../embeddings/doc_embeddings.npy'))
@@ -23,3 +24,24 @@ def test_get_doc_embeddings():
     for i in range(len(docs)):
         assert np.array_equal(doc_embeddings[i], embeddings_index[docs[i]])
         
+def test_avg_sen_len_cranfield():
+    cran_docs = get_cranfield_docs()
+    cran_queries = get_cranfield_queries()
+
+    total_sentences = 0
+    for doc in cran_docs:
+        doc = doc['body'] 
+        segmented_doc = SentenceSegmentation().punkt(doc)
+        total_sentences += len(segmented_doc)
+    
+    total_sentences_query = 0
+    for query in cran_queries:
+        query = query['query']
+        segmented_query = SentenceSegmentation().punkt(query)
+        total_sentences_query += len(segmented_query)
+    
+    avg_sen_len = total_sentences / len(cran_docs)
+    avg_sen_len_query = total_sentences_query / len(cran_queries)
+
+    print("Average Sentence Length in Cranfield Docs: ", avg_sen_len)
+    print("Average Sentence Length in Cranfield Queries: ", avg_sen_len_query)
